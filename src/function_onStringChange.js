1645adc4f4a84c3e09735ab590fe9677d9aa82f0
sam <sam@freighttrust.com>
2020-08-20 04:21:42 -0700

    feat(compliance): countries list partially compliant to ofac

--- src/function_onStringChange.js
+++ src/function_onStringChange.js
@@ -1,13 +1,13 @@
-let Web3 = require('Web3');
+let Web3 = require("Web3");
 
 console.log(Web3.version);
 
- let string = Web3.utils.hexToUtf8(data);
+let string = Web3.utils.hexToUtf8(data);
 console.log(string);
 
-    console.log(Web3.version);
+console.log(Web3.version);
 
-    function onStringChange(e) {
-      let data = Web3.utils.utf8ToHex(e.value);
-      document.getElementById('data').value = data;
-    }
+function onStringChange(e) {
+  let data = Web3.utils.utf8ToHex(e.value);
+  document.getElementById("data").value = data;
+}
--- src/sendMessage.js
+++ src/sendMessage.js
@@ -2,18 +2,19 @@
 // web3.min.js
 // @dev - `sendMessage(string,address)`
 
-    function onAddressChange(e) {
-      let encodedFunctionSignature = web3.eth.abi.encodeFunctionSignature(e.value);
-      console.log(encodedFunctionSignature);
-      document.getElementById('result').innerText = encodedFunctionSignature;
-    }
+function onAddressChange(e) {
+  let encodedFunctionSignature = web3.eth.abi.encodeFunctionSignature(e.value);
+  console.log(encodedFunctionSignature);
+  document.getElementById("result").innerText = encodedFunctionSignature;
+}
 
-    window.onload = function() {
-      if (typeof web3 !== 'undefined') {
-        web3 = new Web3(web3.currentProvider);
-      } else {
-        web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"));
-      }
-      console.log(web3.version);
-    }
-
+window.onload = function () {
+  if (typeof web3 !== "undefined") {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    web3 = new Web3(
+      new Web3.providers.HttpProvider("https://mainnet.infura.io")
+    );
+  }
+  console.log(web3.version);
+};
